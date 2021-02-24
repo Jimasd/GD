544eb03cb652b5d922e69fb1181601b43063963e
Jimasd <37746668+Jimasd@users.noreply.github.com>
2021-02-23 22:20:10 -0500

    Update CondStatement.cs

--- Exercise/CondStatement.cs
+++ Exercise/CondStatement.cs
@@ -1,13 +1,23 @@
+/*
+3. Write a C# Sharp program to check whether a given number is positive or negative. Go to the editor
+Test Data : 14
+Expected Output :
+14 is a positive number
+*/
 public class ExerciseCond3
 {
     public static void Main( )
     {   
         int n = System.Convert.ToInt32(System.Console.ReadLine());
         
-        if (n >= 0) {
-            System.Console.WriteLine("0 ou positif");
+        if (n > 0) {
+            System.Console.WriteLine("Positif");
         } 
-        else 
+        else if (n == 0)
+        {
+            System.Console.WriteLine(0);
+        }
+        else
         {
             System.Console.WriteLine("Negatif");
         }
