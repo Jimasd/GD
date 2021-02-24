735236f3d49af3d4c9fa60863e84e4b8e8ea82a3
Jimasd <37746668+Jimasd@users.noreply.github.com>
2021-02-23 23:08:01 -0500

    Update Array.cs

--- Exercise/Array.cs
+++ Exercise/Array.cs
@@ -1,3 +1,17 @@
+/*
+4. Write a program in C# Sharp to copy the elements one array into another array. Go to the editor
+Test Data :
+Input the number of elements to be stored in the array :3
+Input 3 elements in the array :
+element - 0 : 15
+element - 1 : 10
+element - 2 : 12
+Expected Output:
+The elements stored in the first array are :
+15 10 12
+The elements copied into the second array are :
+15 10 12
+*/
 public class ExerciseArray4
 {
     public static void Main( )
