0b371eb2160056d4480a825bcaa56af20818cdc6
Jimasd <37746668+Jimasd@users.noreply.github.com>
2021-02-23 22:13:28 -0500

    Create CondStatement.cs

---
+++ Exercise/CondStatement.cs
@@ -0,0 +1,15 @@
+public class ExerciseCond3
+{
+    public static void Main( )
+    {   
+        int n = System.Convert.ToInt32(System.Console.ReadLine());
+        
+        if (n >= 0) {
+            System.Console.WriteLine("0 ou positif");
+        } 
+        else 
+        {
+            System.Console.WriteLine("Negatif");
+        }
+    }
+}
