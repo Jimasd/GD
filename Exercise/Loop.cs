118beaafe51648458d4c89f88aee995a60c6607d
Jimasd <37746668+Jimasd@users.noreply.github.com>
2021-02-23 20:44:39 -0500

    Update Loop.cs

--- Exercise/Loop.cs
+++ Exercise/Loop.cs
@@ -52,3 +52,31 @@ public class LoopExercise9
          }
     }
 }
+
+/*
+11. Write a program in C# Sharp to make such a pattern like right angle triangle with a number which will repeat a number in a row. Go to the editor
+The pattern like :
+
+1
+22
+333
+4444
+*/
+public class LoopExercise11
+{
+    public static void Main( )
+    {
+    string line = "";
+   
+    for (int i=1; i<=4; i++)
+         {
+            
+             for (int j=1; j<=i; j++)
+             {
+                 line += i;
+             }
+             System.Console.WriteLine(line);
+             line = "";
+         }
+    }
+}
