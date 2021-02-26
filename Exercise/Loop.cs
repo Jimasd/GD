1b613cb56250f8d492b6751e9cb6ab67b5f1c1b3
Jimasd <37746668+Jimasd@users.noreply.github.com>
2021-02-25 19:33:04 -0500

    Update Loop.cs

--- Exercise/Loop.cs
+++ Exercise/Loop.cs
@@ -83,6 +83,9 @@ public class LoopExercise11
 
 /*
 36. Write a program in C# Sharp to display the such a pattern for n number of rows using a number which will start with the number 1 and the first and a last number of each row will be 1. Go to the editor
+  1
+ 121
+12321
 */
 using System;  
 public class LoopExercice36
